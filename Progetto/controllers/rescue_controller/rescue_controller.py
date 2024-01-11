99361cf3ef2ca241c5389c36ec20a8f0058581f7
RossBong <rossbong99@gmail.com>
2024-01-11 17:33:51 +0100

    move e rotate

--- Progetto/controllers/rescue_controller/rescue_controller.py
+++ Progetto/controllers/rescue_controller/rescue_controller.py
@@ -5,8 +5,8 @@
 from controller import Robot, DistanceSensor, Motor
 import math
 
-TIME_STEP = 64
-MAX_SPEED = 6.29
+TIME_STEP = 32
+MAX_SPEED = 4
 
 # create the Robot instance.
 robot = Robot()
@@ -30,11 +30,19 @@ left_ps.enable(timestep)
 right_ps= robot.getDevice('right wheel sensor')
 right_ps.enable(timestep)
 
+
+#enable imu
+imu = robot.getDevice('inertial unit')
+imu.enable(timestep)
+
+
 ps_values=[0, 0]
 dist_values=[0,0]
 
-raggio_ruota=0.195/2
-dist_ruote=0.192
+raggio_ruota=0.19/2
+
+dist_ruote=0.33205
+d_mid=dist_ruote/2
 
 circonf_ruota=raggio_ruota*2*3.14  #0.61 m
 enc_unit=circonf_ruota/6.29
@@ -73,11 +81,12 @@ def robot_position():
     dist_values[0]=ps_values[0]*enc_unit
     dist_values[1]=ps_values[1]*enc_unit
     print("distance value:"+str(dist_values[0])+" "+str(dist_values[1]))
-    
+    q=(imu.getRollPitchYaw()[2] * 180) / 3.14159
+    print(f"imu:{q}")
     
     
 def get_time(distance, speed):
-    rad=distance/circonf_ruota
+    rad=(distance*2*3.14)/(circonf_ruota)
     return rad/speed
     
 def stop_motors():
@@ -88,6 +97,7 @@ def stop_motors():
 def move(dist):
     seconds = get_time(dist, MAX_SPEED)
     end_time = seconds + robot.getTime()
+    print(f"end time:{end_time}")
     
     while robot.step(TIME_STEP) != -1:
        
@@ -103,8 +113,41 @@ def move(dist):
             stop_motors()
             break
             
+            
+def get_rot_speed_rad(degrees, seconds, raggio_ruota, d_mid):
+    circle = d_mid * 2 * math.pi
+    dist = (degrees / 360) * circle
+    linear_vel = dist / seconds
+    left_wheel_speed = linear_vel / raggio_ruota
+    right_wheel_speed = -1 * linear_vel /raggio_ruota
+    return left_wheel_speed, right_wheel_speed
+    
+               
+def rotate(degrees, seconds, direction):
+
+  
+    # get the left and right rotaional speeds to turn x degrees in y seconds
+    left, right = get_rot_speed_rad(degrees, seconds, raggio_ruota, d_mid)
+    end_time = seconds + robot.getTime()
+    
+    while robot.step(TIME_STEP) != -1:
+        
+        robot_position()
+        
+     
+        print(f"Rotating {direction}...")
+        if robot.getTime() < end_time:
+            leftMotor.setVelocity(left)
+            rightMotor.setVelocity(right)
+        else:
+            stop_motors()
+            break    
+
+            
+            
 def main():
-    move(1.0)
+    rotate(-90, 1.5, "right")
+     #move(1.0)
 
     
 if __name__ == "__main__":
