a42f1c5976ab1d34619002f2a2e7094f68a4d195
RossBong <rossbong99@gmail.com>
2024-01-14 18:05:29 +0100

    a

--- Progetto/controllers/rescue_controller/rescue_controller.py
+++ Progetto/controllers/rescue_controller/rescue_controller.py
@@ -12,7 +12,8 @@ MAX_SPEED = 4
 robot = Robot()
 
 # get the time step of the current world.
-timestep = TIME_STEP
+
+timestep = int(robot.getBasicTimeStep())
 
 leftMotor = robot.getDevice('left wheel')
 rightMotor = robot.getDevice('right wheel')
@@ -30,7 +31,17 @@ left_ps.enable(timestep)
 right_ps= robot.getDevice('right wheel sensor')
 right_ps.enable(timestep)
 
-
+#distance sensor instance
+frontsx= robot.getDevice('so3')
+frontdx= robot.getDevice('so4')
+frontsx.enable(timestep)
+frontdx.enable(timestep)
+backdx= robot.getDevice('so11')
+backsx= robot.getDevice('so12')
+backsx.enable(timestep)
+backdx.enable(timestep)
+ds1=robot.getDevice('ds1')
+ds1.enable(timestep)
 #enable imu
 imu = robot.getDevice('inertial unit')
 imu.enable(timestep)
@@ -44,13 +55,15 @@ raggio_ruota=0.19/2
 dist_ruote=0.33205
 d_mid=dist_ruote/2
 
-circonf_ruota=raggio_ruota*2*3.14  #0.61 m
+dist_value=[0,0,0,0,0]
+
+circonf_ruota=raggio_ruota*2*math.pi #0.61 m
 enc_unit=circonf_ruota/6.29 #porzione di circonferenza per radiante
 
 robot_pose=[0,0," "]
-last_ps_values=[0,0]
+#last_ps_values=[0,0]
 
-
+"""
 def robot_position():
     ps_values[0]=left_ps.getValue()
     ps_values[1]=right_ps.getValue()
@@ -62,7 +75,7 @@ def robot_position():
     dist_values[1]=ps_values[1]*enc_unit
     print("distance value:"+str(dist_values[0])+" "+str(dist_values[1]))
     q=(imu.getRollPitchYaw()[2] * 180) / 3.14159
-    print(f"imu:{q}")
+    print(f"imu:{q}")"""
     
 def direction():
     q=(imu.getRollPitchYaw()[2] * 180) / 3.14159
@@ -93,9 +106,41 @@ def robot_update(dist):
         elif(dir=="South"):
             robot_pose[0]-=dist
     print(f"posizione:({robot_pose[0]},{robot_pose[1]}), direzione:{robot_pose[2]}")
-
+"""
+def raddrizza():
     
-    
+       global dist_value
+       dist_value[0]=frontdx.getValue()/1000
+       dist_value[1]=frontsx.getValue()/1000
+       dist_value[2]=backdx.getValue()/1000
+       dist_value[3]=backsx.getValue()/1000
+      
+       print(dist_value)
+       if(dist_value[0]<0.257):
+           move(0.257-dist_value[0],False)
+           print(f"Ci siamo mossi di {0.257-dist_value[0]} in dietro")
+       elif(dist_value[1]<0.257):
+           move(0.257-dist_value[1],False)
+           print(f"Ci siamo mossi di {0.257-dist_value[1]} in dietro")
+       elif(dist_value[2]<0.257):
+           move(0.257-dist_value[2],True)
+           print(f"Ci siamo mossi di {0.257-dist_value[2]} in avanti")    
+  
+       elif(dist_value[3]<0.257):
+           move(0.257-dist_value[3],True)
+           print(f"Ci siamo mossi di {0.257-dist_value[3]} in avanti")
+       
+ """          
+     
+def distancesensor():
+  
+       global dist_value
+       dist_value[0]=frontdx.getValue()
+       dist_value[1]=frontsx.getValue()
+       dist_value[2]=backdx.getValue()
+       dist_value[3]=backsx.getValue()
+       dist_value[4]=backsx.getValue()
+       print(dist_value)
     
 def get_time(distance, speed):
     rad=(distance*2*3.14)/(circonf_ruota)
@@ -106,22 +151,26 @@ def stop_motors():
     rightMotor.setVelocity(0)
     print("Motors stopped.")
     
-def move(dist):
+def move(dist,forward):
+    maxspeed=MAX_SPEED
     seconds = get_time(dist, MAX_SPEED)
     end_time = seconds + robot.getTime()
     print(f"Moving {dist} m forward...")
-    
+    if(forward==False):
+        maxspeed=-MAX_SPEED
     while robot.step(TIME_STEP) != -1:
-        
-        
+        distancesensor()
         if robot.getTime() < end_time:
-            leftMotor.setVelocity(MAX_SPEED)
-            rightMotor.setVelocity(MAX_SPEED)
+            leftMotor.setVelocity(maxspeed)
+            rightMotor.setVelocity(maxspeed)
             
         else:
             stop_motors()
             break
-    robot_update(dist)
+    if(dist>0.257):        
+        robot_update(dist)
+    
+  
             
             
 def get_rot_speed_rad(degrees, seconds, raggio_ruota, d_mid):
@@ -135,41 +184,54 @@ def get_rot_speed_rad(degrees, seconds, raggio_ruota, d_mid):
                
 def rotate(degrees, seconds, direction):
 
-  
+    
     # get the left and right rotaional speeds to turn x degrees in y seconds
     left, right = get_rot_speed_rad(degrees, seconds, raggio_ruota, d_mid)
     end_time = seconds + robot.getTime()
     print(f"Rotating {direction}...")
+    
     while robot.step(TIME_STEP) != -1:
-       
+        
         if robot.getTime() < end_time:
             leftMotor.setVelocity(left)
             rightMotor.setVelocity(right)
         else:
             stop_motors()
-            break    
+            break
+            
     robot_update(0)
+    
             
-            
+def raggiungi():
+    move(3.0,True)
+   
+    rotate(90, 1.5, "right")
+    move(1.0,True)
+ 
+    rotate(-90, 1.5, "right")
+    
+    move(1.0,True)
+    
+    rotate(180, 3, "right")
+    move(2.0,True)
+    rotate(90, 1.5, "right")
+    
+    move(1.0,True)
+    rotate(-90, 1.5, "right")
+    move(2.0,True)
+    rotate(180, 3, "right")
+    
+                  
 def main():
-    move(3.0)
-    
-    rotate(90, 1.5, "right")
-    move(1.0)
-    rotate(-90, 1.5, "right")
-    
-    move(1.0)
-    
-    rotate(180, 1.5, "right")
-    move(2.0)
-    rotate(90, 1.5, "right")
-    
-    move(1.0)
-    rotate(-90, 1.5, "right")
-    move(2.0)
-    rotate(180, 1.5, "right")
 
-    
+    while robot.step(TIME_STEP) != -1:
+      
+          raggiungi()
+          #rotate(90, 1.5, "right")
+          
+           
+
+
 if __name__ == "__main__":
     main()
 
